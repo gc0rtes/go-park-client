e0e8e8b7618c09eb54fb39a43870e3d2a8458a73
gc0rtes <guilhermeco@gmail.com>
2021-07-09 14:57:21 +0200

    heroku link

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,4 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl =
+  process.env.API_URL || "https://go-park-server.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const DEFAULT_PAGINATION_LIMIT = 10;
